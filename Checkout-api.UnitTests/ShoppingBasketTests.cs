025466e1d83dd15b6f2aea7ab1ec1f0ce3608998
Chris Hunter <chris-2k6@hotmail.co.uk>
2021-11-21 23:57:26 +0000

    Add tests for CheckoutItems endpoint

--- Checkout-api.UnitTests/ShoppingBasketTests.cs
+++ Checkout-api.UnitTests/ShoppingBasketTests.cs
@@ -11,9 +11,17 @@ namespace Checkout_api.UnitTests
     public class ShoppingBasketTests
     {
         private readonly ShoppingBasketController shoppingBasketController;
-
-
         private readonly ICheckoutService _checkoutService;
+
+        private static Item itemA = new Item { Name = "A", UnitPrice = 10 };
+        private static Item itemB = new Item { Name = "B", UnitPrice = 15 };
+        private static Item itemC = new Item { Name = "C", UnitPrice = 40 };
+        private static Item itemD = new Item { Name = "D", UnitPrice = 55 };
+
+        private readonly Basket singleItemBasket = new Basket
+        {
+            Items = new List<Item> { itemA }
+        };
 
         public ShoppingBasketTests()
         {
@@ -38,8 +46,29 @@ namespace Checkout_api.UnitTests
             var result = shoppingBasketController.Get() as OkObjectResult;
 
             // Assert
-            var items = Assert.IsType<List<Item>>(result.Value);
+            var items = Assert.IsType<List<Item>>(result?.Value);
             Assert.Equal(4, items.Count);
+        }
+
+        [Fact]
+        public void Checkout_WithValidItems_ReturnOkActionResult()
+        {
+            // Act
+            var result = shoppingBasketController.CheckoutItems(singleItemBasket) as OkObjectResult;
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result as OkObjectResult);
+        }
+
+        [Fact]
+        public void Checkout_WithSingleItem_BasketTotalOf10()
+        {
+            // Act
+            var result = shoppingBasketController.CheckoutItems(singleItemBasket) as OkObjectResult;
+
+            // Assert
+            var basket = Assert.IsType<Basket>(result?.Value);
+            Assert.Equal(10, basket.Total);
         }
     }
 }
